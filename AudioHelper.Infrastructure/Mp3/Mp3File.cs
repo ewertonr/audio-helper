82eb29c991df796a36966d4950ce84dd1a20d904
Ewerton Ribeiro <ewertonrdoria@hotmail.com>
2018-08-27 20:03:30 -0300

    :sparkles: implements the MP3 contract using NAudio library

---
+++ AudioHelper.Infrastructure/Mp3/Mp3File.cs
@@ -0,0 +1,29 @@
+﻿using AudioHelper.Domain.Contract;
+using NAudio.Wave;
+using System;
+
+namespace AudioHelper.Infrastructure.Mp3
+{
+	public class Mp3File : IMp3File
+	{
+		private readonly Mp3FileReader mp3FileReader;
+
+		public Mp3File(string mp3Path)
+		{
+			if (string.IsNullOrEmpty(mp3Path)) { throw new ArgumentNullException(nameof(mp3Path)); }
+
+			this.mp3FileReader = new Mp3FileReader(mp3Path);			
+		}
+
+		public TimeSpan CurrentTime => this.mp3FileReader.CurrentTime;
+
+		public TimeSpan TotalTime => this.mp3FileReader.TotalTime;
+
+		public byte[] RawData => this.mp3FileReader.ReadNextFrame()?.RawData;
+
+		public void Dispose()
+		{
+			this.mp3FileReader.Dispose();
+		}
+	}
+}
