05563d2145b0ea3c3908513b688b0eebf839a0f1
Ewerton Ribeiro <ewertonrdoria@hotmail.com>
2018-08-27 20:02:29 -0300

    :sparkles: creates a contract that represents a MP3 file

---
+++ AudioHelper.Domain.Contract/IMp3File.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AudioHelper.Domain.Contract
+{
+	public interface IMp3File : IDisposable
+	{
+		TimeSpan CurrentTime { get; }
+
+		TimeSpan TotalTime { get; }
+
+		byte[] RawData { get; }
+	}
+}
