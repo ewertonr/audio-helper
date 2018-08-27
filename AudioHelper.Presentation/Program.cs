6d158ce59f453308671dc4bf6f87b7ee6e1eca9b
Ewerton Ribeiro <ewertonrdoria@hotmail.com>
2018-08-27 20:06:25 -0300

    :sparkles: creates a presentation layer to use the AudioHelper as command line

---
+++ AudioHelper.Presentation/Program.cs
@@ -0,0 +1,28 @@
+﻿using AudioHelper.Domain;
+using AudioHelper.Infrastructure.FileSystem;
+using System;
+
+namespace AudioHelper.Presentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+				var factory = new SongFactory(new FileSystem());
+
+				factory.ReadFile(@"C:\test_aud\musics.txt", @"C:\test_aud\vou_pro_sereno.mp3");
+				factory.GenerateSongs();
+
+				Console.WriteLine("Hello World!");
+				Console.ReadKey();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				Console.ReadKey();
+			}
+        }
+    }
+}
