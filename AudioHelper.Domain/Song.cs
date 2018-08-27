02ae43404570692443ed4a9f9836e22ec62874de
Ewerton Ribeiro <ewertonrdoria@hotmail.com>
2018-08-27 19:59:52 -0300

    :sparkles: creates a representation and logic for generate songs

---
+++ AudioHelper.Domain/Song.cs
@@ -0,0 +1,48 @@
+﻿using AudioHelper.Domain.Contract;
+using AudioHelper.Infrastructure.Mp3;
+using System;
+using System.IO;
+
+namespace AudioHelper.Domain
+{
+	public class Song
+	{
+		private readonly IMp3File mp3File;
+
+		public Song(string mp3Path, int order, string title, TimeSpan start, TimeSpan end)
+		{
+			this.mp3File = new Mp3File(mp3Path);
+
+			this.Title = $"{order} - {title}.mp3";
+
+			this.Start = start;
+			this.End = end == TimeSpan.Zero
+				? this.mp3File.TotalTime
+				: end;
+		}
+
+		private string Title { get; }
+
+		private byte[] RawData => this.mp3File.RawData;
+
+		private TimeSpan CurrentTime => this.mp3File.CurrentTime;
+
+		private TimeSpan End { get; }
+
+		private TimeSpan Start { get; }
+
+		private bool IsInInterval => this.CurrentTime >= this.Start && this.CurrentTime <= this.End;
+
+		public void GenerateMp3(string path)
+		{
+			using (var writer = File.Create($@"{path}\{this.Title}"))
+			{
+				byte[] frame = null;
+				while ((frame = this.RawData) != null)
+				{
+					if (this.IsInInterval) { writer.Write(frame, 0, frame.Length); }
+				}
+			}
+		}
+	}
+}
---
+++ AudioHelper.Domain/SongFactory.cs
@@ -0,0 +1,72 @@
+﻿using AudioHelper.Infrastructure.FileSystem;
+using System;
+using System.Collections.Generic;
+
+namespace AudioHelper.Domain
+{
+	public class SongFactory
+	{
+		private readonly IFileSystemWrapper fileSystem;
+		private string mp3FilePath;
+
+		public SongFactory(IFileSystemWrapper fileSystem)
+		{
+			this.fileSystem = fileSystem ?? throw new ArgumentNullException(nameof(fileSystem));
+		}
+
+		private readonly ICollection<Song> Songs = new List<Song>();
+
+		public void ReadFile(string path, string mp3FilePath)
+		{
+			this.mp3FilePath = path;
+
+			var lines = this.fileSystem.ReadAllLines(path);
+
+			for (int i = 0; i < lines.Length; i++)
+			{
+				var line = lines[i];
+				if (string.IsNullOrEmpty(line)) { continue; }
+
+				var order = i + 1;
+				var title = this.GetTitle(line);
+				var start = this.GetTimeSpan(line);
+
+				var end = i == lines.Length - 1
+					? TimeSpan.Zero
+					: this.GetTimeSpan(lines[i + 1]);
+
+				var song = new Song(mp3FilePath, order, title, start, end);
+				this.Songs.Add(song);
+			}
+		}
+
+		public void GenerateSongs()
+		{
+			if (this.Songs.Count == 0) { throw new Exception("Não há nenhuma música para ser gerada. Verifique e tente novamente."); }
+
+			var outputDirectory = $@"{this.fileSystem.DirectoryName(this.mp3FilePath)}\cd\";
+			this.fileSystem.CreateDirectory(outputDirectory);
+
+			foreach (var music in this.Songs)
+			{
+				music.GenerateMp3(outputDirectory);
+			}
+
+			this.Songs.Clear();
+		}
+
+		private string GetTitle(string line)
+		{
+			int count = line.IndexOf("- ") + 2;
+			return line.Substring(count);
+		}
+
+		private TimeSpan GetTimeSpan(string line)
+		{
+			int count = line.IndexOf(" -");
+			var time = line.Substring(0, count);
+
+			return TimeSpan.Parse(time);
+		}
+	}
+}
